7423f6c00015ab9c8562f12005dc21c67dea6548
nrasheva <nadezhda.rasheva96@gmail.com>
2023-12-10 12:36:16 +0200

    Scaffold project with Nest

--- router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getCocktail, getCocktails } = require('./handlers/cocktails');
-
-const router = express.Router();
-
-router.get('/getCocktail', getCocktail);
-router.get('/getCocktails', getCocktails);
-
-module.exports = router;
