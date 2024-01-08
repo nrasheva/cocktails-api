5b1d15f01c1e14a3365ec85a3de2ee514c032dac
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-01-08 17:47:32 +0200

    Implement Cocktail class-validator

--- src/cocktails/cocktails.dto.ts
+++ src/cocktails/cocktails.dto.ts
@@ -1,6 +1,30 @@
-import { IsString } from 'class-validator';
+import { IsAlpha, IsArray, IsString, IsUrl } from 'class-validator';
 
 export class CreateCocktailDto {
   @IsString()
   name: string;
+
+  @IsString()
+  description: string;
+
+  @IsUrl()
+  img: string;
+
+  @IsArray()
+  ingredients: string[][];
+
+  @IsArray()
+  @IsString({ each: true })
+  recipe: string[];
+
+  @IsString()
+  @IsAlpha()
+  taste: string;
+
+  @IsString()
+  time: string;
+
+  @IsString()
+  @IsAlpha()
+  level: string;
 }
