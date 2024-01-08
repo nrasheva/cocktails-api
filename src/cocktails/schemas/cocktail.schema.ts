e1c2a35619dde2ba8037f56917761600a3b231fd
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-01-08 17:46:28 +0200

    Elaborate Cocktail Schema

--- src/cocktails/schemas/cocktail.schema.ts
+++ src/cocktails/schemas/cocktail.schema.ts
@@ -3,10 +3,47 @@ import { HydratedDocument } from 'mongoose';
 
 export type CocktailDocument = HydratedDocument<Cocktail>;
 
+class Ingredient {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  quantity: string;
+}
+
+class RecipeStep {
+  @Prop({ required: true })
+  stepNumber: string;
+
+  @Prop({ required: true })
+  instruction: string;
+}
+
 @Schema()
 export class Cocktail {
-  @Prop()
+  @Prop({ required: true })
   name: string;
+
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({ required: true })
+  img: string;
+
+  @Prop({ type: [Ingredient], required: true })
+  ingredients: Ingredient[];
+
+  @Prop({ type: [RecipeStep], required: true })
+  recipe: RecipeStep[];
+
+  @Prop({ required: true })
+  taste: string;
+
+  @Prop({ required: true })
+  time: string;
+
+  @Prop({ required: true })
+  level: string;
 }
 
 export const CocktailSchema = SchemaFactory.createForClass(Cocktail);
