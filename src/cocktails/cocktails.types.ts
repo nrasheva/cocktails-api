c67a047c55a85bcf37b40481a2e656ed2531fa2d
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-01-08 17:46:45 +0200

    Elaborate Cocktail type

--- src/cocktails/cocktails.types.ts
+++ src/cocktails/cocktails.types.ts
@@ -1,3 +1,20 @@
+export type Ingredient = {
+  name: string;
+  quantity: string;
+};
+
+export type RecipeStep = {
+  stepNumber: string;
+  instruction: string;
+};
+
 export type Cocktail = {
   name: string;
+  description: string;
+  img: string;
+  ingredients: Ingredient[];
+  recipe: RecipeStep[];
+  taste: string;
+  time: string;
+  level: string;
 };
