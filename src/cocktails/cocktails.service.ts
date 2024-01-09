12aa433a78ff2839cfca6a8f1da13be928f19f18
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-01-10 00:21:30 +0200

    Implementing a bulk insert on sending a GET request

--- src/cocktails/cocktails.service.ts
+++ src/cocktails/cocktails.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
+import { COCKTAILS_DATA } from './cocktails.data';
 import { CreateCocktailDto } from './cocktails.dto';
 import { Cocktail } from './schemas/cocktail.schema';
 
@@ -17,5 +18,21 @@ export class CocktailsService {
 
   async findAll(): Promise<Cocktail[]> {
     return this.cocktailModel.find().exec();
+  }
+
+  async bulkInsert(cocktailsData: CreateCocktailDto[]): Promise<void> {
+    await this.cocktailModel.insertMany(cocktailsData);
+  }
+
+  async initializeData(): Promise<void> {
+    for (const cocktailData of COCKTAILS_DATA) {
+      // Check if a cocktail with the same unique identifier already exists
+      const existingCocktail = await this.cocktailModel.findOne({ name: cocktailData.name }).exec();
+
+      if (!existingCocktail) {
+        const createdCocktail = new this.cocktailModel(cocktailData);
+        await createdCocktail.save();
+      }
+    }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { CocktailsService } from './cocktails/cocktails.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const cocktailsService = app.get(CocktailsService);
+
+  await cocktailsService.initializeData();
 
   await app.listen(3000);
 }
