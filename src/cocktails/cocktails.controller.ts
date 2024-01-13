ea6fc20b77addbdbba1cd0ebd3c1ec7b44cbde30
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-01-13 20:04:08 +0200

    Implementing Cocktail edit controller

--- src/cocktails/cocktails.controller.ts
+++ src/cocktails/cocktails.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Get, NotFoundException, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, NotFoundException, Post, Put, Query } from '@nestjs/common';
 
-import { CreateCocktailDto } from './cocktails.dto';
+import { CreateCocktailDto, UpdateCocktailDto } from './cocktails.dto';
 import { CocktailsService } from './cocktails.service';
 
 import type { Cocktail } from './cocktails.types';
@@ -26,5 +26,18 @@ export class CocktailsController {
       throw new NotFoundException(`Cocktail with ID ${id} not found`);
     }
     return cocktail;
+  }
+
+  @Put('edit')
+  async update(@Query('id') id: string, @Body() updateCocktailDto: UpdateCocktailDto) {
+    try {
+      const updatedCocktail = await this.cocktailsService.findByIdAndUpdate(id, updateCocktailDto);
+      if (!updatedCocktail) {
+        throw new HttpException('Cocktail not found', HttpStatus.NOT_FOUND);
+      }
+      return updatedCocktail;
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
   }
 }
