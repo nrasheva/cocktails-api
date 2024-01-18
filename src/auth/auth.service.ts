27f5419ee0e44303983548fed357507c4aac5646
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-01-18 21:21:31 +0200

    Implementing Auth service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,40 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
+import { UsersService } from 'src/users/users.service';
+
+import { CreateUserDto } from './auth.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async register(userDto: CreateUserDto): Promise<any> {
+    const existingUser = await this.usersService.findOne(userDto.email);
+    if (existingUser) {
+      throw new Error('User already exists');
+    }
+    return this.usersService.create(userDto);
+  }
+
+  async signIn(email: string, pass: string) {
+    const user = await this.usersService.findOne(email);
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
+
+    const isMatch = await bcrypt.compare(pass, user.password);
+    if (!isMatch) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const payload = { email: user?.email };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+}
