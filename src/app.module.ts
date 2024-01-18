f8acfcf4d7442fe7fafb98a8efcafe0bcf313229
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-01-18 21:23:58 +0200

    Update App module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { CocktailsModule } from './cocktails/cocktails.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CocktailsModule, MongooseModule.forRoot('mongodb://localhost:27017/cocktails')],
+  imports: [CocktailsModule, MongooseModule.forRoot('mongodb://localhost:27017/cocktails'), AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
