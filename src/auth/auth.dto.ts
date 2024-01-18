cb04199d7f3e35afe1b8eafd01e51441fdb58e90
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-01-18 21:22:41 +0200

    Implementing Auth dto

---
+++ src/auth/auth.dto.ts
@@ -0,0 +1,19 @@
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
+export class signInDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(8)
+  password: string;
+}
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(8)
+  password: string;
+}
