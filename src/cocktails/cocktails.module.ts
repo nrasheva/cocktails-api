b3e69a904cf5dfeb03200d59068248b2250d42d8
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-02-05 17:39:20 +0200

    Update cocktails module

--- src/cocktails/cocktails.module.ts
+++ src/cocktails/cocktails.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
+import { AuthGuard } from 'src/auth/auth.guard';
+import { UsersModule } from 'src/users/users.module';
+
 import { CocktailsController } from './cocktails.controller';
 import { CocktailsService } from './cocktails.service';
 import { Cocktail, CocktailSchema } from './schemas/cocktail.schema';
 
 @Module({
   controllers: [CocktailsController],
-  imports: [MongooseModule.forFeature([{ name: Cocktail.name, schema: CocktailSchema }])],
-  providers: [CocktailsService],
+  imports: [MongooseModule.forFeature([{ name: Cocktail.name, schema: CocktailSchema }]), UsersModule],
+  providers: [CocktailsService, AuthGuard],
 })
 export class CocktailsModule {}
