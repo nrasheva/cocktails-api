ead941bf9f547c22661fa45474ecf800e684d8e3
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2025-05-09 12:00:35 +0300

    Updated app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { CocktailsModule } from './cocktails/cocktails.module';
+import { FavoritesModule } from './favorites/favorites.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CocktailsModule, MongooseModule.forRoot('mongodb://localhost:27017/cocktails'), AuthModule, UsersModule],
+  imports: [
+    CocktailsModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/cocktails'),
+    AuthModule,
+    UsersModule,
+    FavoritesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
