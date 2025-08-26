97fcddefff60189ff05e7b3802ed9cb539c3d4c5
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2025-08-26 11:45:47 +0300

    Updated user schema

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -17,8 +17,11 @@ export class User extends Document {
   @Prop({ required: true })
   password: string;
 
-  @Prop([String])
+  @Prop({ type: [String], default: ['user'] })
   roles: string[];
+
+  @Prop({ type: String, default: null })
+  refreshToken?: string | null;
 
   @Prop({ type: [{ type: Types.ObjectId, ref: 'Cocktail' }] })
   favorites: Types.ObjectId[];
