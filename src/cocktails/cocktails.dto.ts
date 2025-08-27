51f94c8c0a1068b4e983efd90d47c11828b71240
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2025-08-27 15:38:44 +0300

    Updated cocktails dto

--- src/cocktails/cocktails.dto.ts
+++ src/cocktails/cocktails.dto.ts
@@ -1,70 +1,100 @@
 import { Type } from 'class-transformer';
-import { IsAlpha, IsArray, IsString, IsUrl, ValidateNested } from 'class-validator';
+import { IsArray, IsString, IsUrl, ValidateNested } from 'class-validator';
 
 class IngredientDto {
   @IsString()
   name: string;
 
   @IsString()
-  quantity: string;
+  measure: string;
 }
 
 export class CreateCocktailDto {
   @IsString()
+  idDrink: string;
+
+  @IsString()
   name: string;
 
   @IsString()
-  description: string;
+  category: string;
+
+  @IsString()
+  alcoholic: string;
+
+  @IsString()
+  glass: string;
+
+  @IsString()
+  instructions: string;
 
   @IsUrl()
   img: string;
+
+  @IsUrl()
+  thumb: string;
 
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => IngredientDto)
   ingredients: IngredientDto[];
 
-  @IsArray()
-  @IsString({ each: true })
-  recipe: string[];
+  // @IsArray()
+  // @IsString({ each: true })
+  // recipe: string[];
 
-  @IsString()
-  @IsAlpha()
-  taste: string;
+  // @IsString()
+  // @IsAlpha()
+  // taste: string;
 
-  @IsString()
-  time: string;
+  // @IsString()
+  // time: string;
 
-  @IsString()
-  @IsAlpha()
-  level: string;
+  // @IsString()
+  // @IsAlpha()
+  // level: string;
 }
 
 export class UpdateCocktailDto {
   @IsString()
+  idDrink: string;
+
+  @IsString()
   name: string;
 
   @IsString()
-  description: string;
+  category: string;
+
+  @IsString()
+  alcoholic: string;
+
+  @IsString()
+  glass: string;
+
+  @IsString()
+  instructions: string;
 
   @IsUrl()
   img: string;
 
+  @IsUrl()
+  thumb: string;
+
   @IsArray()
   ingredients: string[][];
 
-  @IsArray()
-  @IsString({ each: true })
-  recipe: string[];
+  // @IsArray()
+  // @IsString({ each: true })
+  // recipe: string[];
 
-  @IsString()
-  @IsAlpha()
-  taste: string;
+  // @IsString()
+  // @IsAlpha()
+  // taste: string;
 
-  @IsString()
-  time: string;
+  // @IsString()
+  // time: string;
 
-  @IsString()
-  @IsAlpha()
-  level: string;
+  // @IsString()
+  // @IsAlpha()
+  // level: string;
 }
