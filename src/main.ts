8beefeca8f877fd33992e3b4285be7a9e971e051
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2025-08-27 15:33:50 +0300

    Updated main file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as nodeCrypto from 'crypto';
 
 import { AppModule } from './app.module';
 import { CocktailsService } from './cocktails/cocktails.service';
+
+(global as any).crypto = {
+  randomUUID: nodeCrypto.randomUUID ? nodeCrypto.randomUUID.bind(nodeCrypto) : undefined,
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
